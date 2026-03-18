70a0fa302959a837e8e0d1dd1ef95ef91d30c705
EXTERNAL Ortiz Charles (UABC <orc5mex@bosch.com>
2026-03-18 07:55:32 -0700

    Add User function modal working

--- client/src/components/AddUserModal.jsx
+++ client/src/components/AddUserModal.jsx
@@ -1,21 +1,29 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Modal from "../components/Modal"
 import AddButton from "../components/AddButton"
 import Input from "../components/Input"
 import { useForm } from "react-hook-form"
 
 
-const AddUserModal = ({ isOpen, onClose, onSubmit }) => {
+const AddUserModal = ({ isOpen, onClose, onSubmit, user }) => {
     const { register, handleSubmit, reset } = useForm()
 
+    useEffect(() => {
+        if (user) {
+            reset(user);
+        } else {
+            reset({});
+        }
+    }, [user, reset])
+
     const submitHandler = (data) => {
-        onSubmit(data)
-        reset()
+        onSubmit(data, user)
+        //reset()
         onClose()
     }
 
     return (
-        <Modal isOpen={isOpen} onClose={onClose} title="Add New User">
+        <Modal isOpen={isOpen} onClose={onClose} title={user ? "Edit User" : "Add New User"}>
 
             <form onSubmit={handleSubmit(submitHandler)} className="flex flex-col gap-4">
 
@@ -23,7 +31,7 @@ const AddUserModal = ({ isOpen, onClose, onSubmit }) => {
                 <Input label="Last Name" name="last_name" register={register} />
                 <Input label="Email" name="email" register={register} type="email" />
                 <Input label="Phone" name="phone" register={register} />
-                <Input label="Password" name="password" register={register} />
+                {!user && <Input label="Password" name="password" register={register} />}
                 <Input label="Department" name="department_id" register={register} />
                 <Input label="Role" name="role_id" register={register} />
 
@@ -33,7 +41,7 @@ const AddUserModal = ({ isOpen, onClose, onSubmit }) => {
                         Cancel
                     </AddButton>
                     <AddButton type="submit">
-                        Save
+                        {user ? "Update" : "Save"}
                     </AddButton>
                 </div>
 
--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,56 +1,57 @@
 import React, { useEffect, useState } from 'react'
 import ReTable from '../components/ReTable.jsx';
-import { getUsers, createUser } from '../services/userService.js';
+import { getUsers, createUser, deleteUser, updateUser } from '../services/userService.js';
 import AddButton from '../components/AddButton.jsx';
 import AddUserModal from '../components/AddUserModal.jsx';
-const userColumns = [
-    { header: "First Name", accessor: "first_name" },
-    { header: "Last Name", accessor: "last_name" },
-    { header: "Email", accessor: "email" },
-    { header: "Phone", accessor: "phone" },
-    { header: "Department", accessor: "department_id" },
-    { header: "Role", accessor: "role_id" },
-    {
-        header: "Actions",
-        render: (row) => (
-            // console.log("ROW", row)
-            // return (<span>test</span>)
-            <div className='flex gap-2'>
-                <button
-                    onClick={() => handleEdit(row)}
-                    className='px-3 py-1 text-sm bg-zinc-900 text-white rounded-md hover:bg-zinc-800'>
-                    Edit
-                </button>
-
-                <button
-                    onClick={() => handleDelete(row)}
-                    className='px-3 py-1 text-sm border border-zinc-300 rounded-md hover:bg-zinc-100'>
-                    Delete
-                </button>
-            </div>
-        )
-    }
-]
 
 const Users = () => {
     const [open, setOpen] = useState(false)
     const [users, setUsers] = useState([])
     const [loading, setLoading] = useState(true)
+    const [selectedUser, setSelectedUser] = useState(null);
 
-    const handleCreateUser = async (data) => {
-        console.log("User Data: ", data);
+    const handleEdit = async (user) => {
+        setSelectedUser(user);
+        setOpen(true);
+    }
+
+    const handleDelete = async (user) => {
         try {
-            const newUser = await createUser(data)
-            console.log("Created user: ", newUser);
-            setUsers(prev => [
-                ...prev,
-                newUser
-            ])
-            setOpen(false)
+            await deleteUser(user.id)
+            setUsers(prev => prev.filter(u => u.id !== user.id))
+        } catch (error) {
+            console.error("Error deleting user: ", error.response?.data);
+        }
+    }
+
+    const handleCreateUser = async (data, user) => {
+        console.log("User Data: ", data);
+
+        try {
+            if (user) {
+                const updatedUser = await updateUser(user.id, data);
+                setUsers(prev => prev.map(u => (u.id === user.id ? updatedUser : u)));
+            } else {
+                const newUser = await createUser(data);
+                setUsers(prev => [
+                    ...prev,
+                    newUser
+                ])
+                setOpen(false)
+            }
         } catch (error) {
             console.error("Error creating user: ", error)
         }
     }
+    // try {
+    //     const newUser = await createUser(data)
+    //     console.log("Created user: ", newUser);
+    //     setUsers(prev => [
+    //         ...prev,
+    //         newUser
+    //     ])
+    //     setOpen(false)
+
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -69,12 +70,40 @@ const Users = () => {
     if (loading) {
         return <p>Loading...</p>
     }
+    const userColumns = [
+        { header: "First Name", accessor: "first_name" },
+        { header: "Last Name", accessor: "last_name" },
+        { header: "Email", accessor: "email" },
+        { header: "Phone", accessor: "phone" },
+        { header: "Department", accessor: "department_id" },
+        { header: "Role", accessor: "role_id" },
+        {
+            header: "Actions",
+            render: (row) => (
+                // console.log("ROW", row)
+                // return (<span>test</span>)
+                <div className='flex gap-2'>
+                    <button
+                        onClick={() => handleEdit(row)}
+                        className='px-3 py-1 text-sm bg-zinc-900 text-white rounded-md hover:bg-zinc-800'>
+                        Edit
+                    </button>
+
+                    <button
+                        onClick={() => handleDelete(row)}
+                        className='px-3 py-1 text-sm border border-zinc-300 rounded-md hover:bg-zinc-100'>
+                        Delete
+                    </button>
+                </div>
+            )
+        }
+    ]
     return (
         <div className='flex flex-col'>
             <div className='p-2 flex justify-end border-2 border-green-500'>
-                <AddButton onClick={() => { console.log("Button "), setOpen(true) }}>+ Add User</AddButton>
+                <AddButton onClick={() => { setSelectedUser(null); setOpen(true); }}>+ Add User</AddButton>
             </div>
-            <AddUserModal isOpen={open} onClose={() => setOpen(false)} onSubmit={handleCreateUser} />
+            <AddUserModal isOpen={open} onClose={() => { setOpen(false); setSelectedUser(null); }} onSubmit={handleCreateUser} user={selectedUser} />
             <ReTable
                 columns={userColumns}
                 data={users}
